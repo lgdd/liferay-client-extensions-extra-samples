da08a59fff9cdcd1ef553e27cb0ccd5d7f07f1fb
lgdd <3501902+lgdd@users.noreply.github.com>
2025-03-02 17:50:15 +0100

    fix: add missing env lookup

--- sample-etc-python-fastapi/main.py
+++ sample-etc-python-fastapi/main.py
@@ -14,16 +14,33 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
-def get_config():
-    config = {}
-    dxp_metadata = os.scandir("dxp-metadata")
-    for entry in dxp_metadata:
-        if entry.is_file():
-            file = open(entry.path, "r")
-            config[entry.name] = file.read()
-    return config
+configTreePathsEnvs = ["LIFERAY_ROUTES_DXP", "LIFERAY_ROUTES_CLIENT_EXTENSION"]
+configTreePaths = []
+config = {}
 
-def validate_jwt(token_str, config):
+def init_config():
+    for env in configTreePathsEnvs:
+        configTreePath = os.environ.get(env)
+        if configTreePath is not None:
+            configTreePaths.append(configTreePath)
+
+    if len(configTreePaths) == 0:
+        logger.info("No environment variable found for config [" + ",".join(configTreePathsEnvs) + "]")
+        logger.info("Default config path to './dxp-metadata'")
+        configTreePaths.append("dxp-metadata")
+
+    logger.info("Load config:")
+    config = {}
+    for configTreePath in configTreePaths:
+        dxp_metadata = os.scandir(configTreePath)
+        for entry in dxp_metadata:
+            if entry.is_file():
+                file = open(entry.path, "r")
+                content = file.read()
+                logger.info("- " + entry.name + "=" + content)
+                config[entry.name] = content
+
+def validate_jwt(token_str):
     protocol = config["com.liferay.lxc.dxp.server.protocol"]
     domain = config["com.liferay.lxc.dxp.mainDomain"]
     oauth2_jwks_uri = protocol + "://" + domain + "/o/oauth2/jwks"
@@ -47,7 +64,7 @@ def validate_jwt(token_str, config):
 
     return decoded_token
 
-def validate_client_id(client_id, config):
+def validate_client_id(client_id):
     protocol = config["com.liferay.lxc.dxp.server.protocol"]
     domain = config["com.liferay.lxc.dxp.mainDomain"]
     external_reference_codes = config["liferay.oauth.application.external.reference.codes"]
@@ -64,11 +81,10 @@ def validate_client_id(client_id, config):
 @app.middleware("http")
 async def jwt_middleware(request: Request, call_next, authorization: Annotated[str | None, Header()] = None):
     if request.get("path") != "/ready" and request.get("path") != "/":
-        config = get_config()
         headers = dict(request.get("headers"))
         token = headers[b'authorization'].decode('utf-8').split("Bearer ")[1]
-        decoded_token = validate_jwt(token, config)
-        validate_client_id(decoded_token["client_id"], config)
+        decoded_token = validate_jwt(token)
+        validate_client_id(decoded_token["client_id"])
     response = await call_next(request)
     return response
 
@@ -86,4 +102,5 @@ async def object_action_1(request: Request):
     logger.info(object_entry)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8502, log_level="info", reload=True)
+    init_config()
+    uvicorn.run("main:app", host="0.0.0.0", port=8502, log_level="info")
