cf93a96bc51468dc3ae35d4714eae2ccd270d4ae
lgdd <3501902+lgdd@users.noreply.github.com>
2025-03-03 02:33:15 +0100

    fix: add missing env lookup

--- sample-etc-java-vertx/src/main/java/org/acme/liferay/client/extension/vertx/sample/MainVerticle.java
+++ sample-etc-java-vertx/src/main/java/org/acme/liferay/client/extension/vertx/sample/MainVerticle.java
@@ -14,12 +14,22 @@ import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.handler.BodyHandler;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
+    Map<String, String> configMap = getConfigMap();
     HttpServer server = vertx.createHttpServer();
     WebClient webClient = WebClient.create(vertx);
     Router router = Router.router(vertx);
@@ -61,28 +71,75 @@ public class MainVerticle extends AbstractVerticle {
           ctx.response().putHeader("content-type", "text/plain").end("executed object action 1");
         });
 
+    int serverPort = config().getInteger("server.port", 8082);
+
     server
         .requestHandler(router)
-        .listen(8082)
+        .listen(serverPort)
         .onComplete(http -> {
           if (http.succeeded()) {
             startPromise.complete();
-            _log.info("HTTP server started on port 8082");
+            _log.info("HTTP server started on port " + serverPort);
           } else {
             startPromise.fail(http.cause());
           }
         });
   }
 
+  private Map<String, String> getConfigMap() {
+    final Map<String, String> configMap = new HashMap<>();
+    final List<String> configTreePaths = new ArrayList<>();
+
+    String liferayRoutesDXP = config().getString("LIFERAY_ROUTES_DXP", null);
+    String liferayRoutesClientExtension = config().getString("LIFERAY_ROUTES_CLIENT_EXTENSION",
+        null);
+
+    if (liferayRoutesDXP != null) {
+      configTreePaths.add(liferayRoutesDXP);
+    }
+
+    if (liferayRoutesClientExtension != null) {
+      configTreePaths.add(liferayRoutesClientExtension);
+    }
+
+    config().stream()
+        .filter(keyValue -> keyValue.getKey().contains("liferay"))
+        .forEach(keyValue -> {
+          configMap.put(keyValue.getKey(), (String) keyValue.getValue());
+        });
+
+    for (String configTreePath : configTreePaths) {
+      Path start = Paths.get(configTreePath);
+      try (Stream<Path> walk = Files.walk(start)) {
+        walk
+            .filter(Files::isRegularFile)
+            .forEach(path -> {
+              try {
+                String content = Files.readString(path, StandardCharsets.UTF_8);
+                System.out.println(path.getFileName().toString() + ": " + content);
+                configMap.put(path.getFileName().toString(), content);
+              } catch (IOException e) {
+                _log.error(e);
+              }
+            });
+      } catch (IOException e) {
+        _log.error(e);
+      }
+    }
+
+    return configMap;
+  }
+
   private void validateJWT(RoutingContext ctx, WebClient webClient) {
+    Map<String, String> configMap = getConfigMap();
     final String token = ctx.request().headers().get("Authorization").split("Bearer ")[1];
     final String externalReferenceCode =
-        config()
-            .getString("liferay.oauth.application.external.reference.codes")
+        configMap
+            .get("liferay.oauth.application.external.reference.codes")
             .split(",")[0];
     final String liferayDomain =
-        config()
-            .getString("com.liferay.lxc.dxp.mainDomain", "localhost:8080");
+        configMap
+            .getOrDefault("com.liferay.lxc.dxp.mainDomain", "localhost:8080");
     final String liferayHost = liferayDomain.split(":")[0];
     final int liferayPort = Integer.parseInt(liferayDomain.split(":")[1]);
 
@@ -129,13 +186,14 @@ public class MainVerticle extends AbstractVerticle {
   }
 
   private void validateClientId(RoutingContext ctx, WebClient webClient, String tokenClientId) {
+    Map<String, String> configMap = getConfigMap();
     final String externalReferenceCode =
-        config()
-            .getString("liferay.oauth.application.external.reference.codes")
+        configMap
+            .get("liferay.oauth.application.external.reference.codes")
             .split(",")[0];
     final String liferayDomain =
-        config()
-            .getString("com.liferay.lxc.dxp.mainDomain", "localhost:8080");
+        configMap
+            .getOrDefault("com.liferay.lxc.dxp.mainDomain", "localhost:8080");
     final String liferayHost = liferayDomain.split(":")[0];
     final int liferayPort = Integer.parseInt(liferayDomain.split(":")[1]);
     final String oauth2ApplicationURI =
